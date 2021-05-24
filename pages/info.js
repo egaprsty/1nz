53b92c12f47e87a8b88b24dc72b12418cebd5022
Ega Prasetya <54002683+egaprsty@users.noreply.github.com>
2021-05-24 11:21:02 +0700

    Update info.js

--- pages/info.js
+++ pages/info.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Head from 'next/head';
 
 function Info() {
     return (
         <div>
+            <Head>
+                <title>Info</title>
+            </Head>
             <p>Under maintened</p>
         </div>
     )
